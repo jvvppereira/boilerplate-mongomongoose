f515e938e1169a3596432686b593f2466d98db39
João Vítor <joao.paes@philips.com>
2022-02-28 19:55:24 -0300

    fix the importation of mongo

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
