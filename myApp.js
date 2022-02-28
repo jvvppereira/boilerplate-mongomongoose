8019fa73c0371da0cc14e6b1b99c467ff172d9db
João Vítor <joao.paes@philips.com>
2022-02-28 19:59:41 -0300

    changes way to connect with mongoose

--- myApp.js
+++ myApp.js
@@ -1,14 +1,8 @@
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
