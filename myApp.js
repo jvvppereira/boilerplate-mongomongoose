d9596a70330845ba2ccce320d5c9815327c972d9
João Vítor <joao.paes@philips.com>
2022-03-06 18:36:53 -0300

    fixed the field name on filter

--- myApp.js
+++ myApp.js
@@ -70,7 +70,7 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  Person.find({food: foodToSearch}).sort({ name: 'asc' }).limit(2).select('-age').exec(function (err, data) { defaultProcess(done, err, data) });
+  Person.find({favoriteFoods: foodToSearch}).sort({ name: 'asc' }).limit(2).select('-age').exec(function (err, data) { defaultProcess(done, err, data) });
 };
 
 /** **Well Done !!**
